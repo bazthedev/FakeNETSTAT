5a3454e62795b23a58e3c50e4da620d8956e1b4a
Baz <bazthedev@gmail.com>
2022-04-09 17:48:30 +0100

    Create compile.bat

---
+++ compile.bat
@@ -0,0 +1,7 @@
+REM Made by bazthedev
+REM Create the executable version from the python script
+py -m pip install autopytoexe
+REM Allow this to finish
+REM If you don't have python installed, go to https://python.org/download
+py -m autopytoexe
+REM Go to settings and type import from JSON config and find the json file with the contents of config.json
